6cc58137a0e0e4988a2c2838f36e6428ba0f7a5a
Omkar <omkarpadalkar21@gmail.com>
2025-06-19 19:38:48 +0530

    Add Tiptap Extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,16 +1,25 @@
 "use client"
 import {useEditor, EditorContent} from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import {ImageResize} from "tiptap-extension-resize-image";
 const Editor = () => {
     const editor = useEditor({
         editorProps: {
             attributes: {
-                style:"padding-left:56px; padding-right:56px;",
+                style: "padding-left:56px; padding-right:56px;",
                 class: "focus:outline-none print:border-0 bg-white border   border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
             }
         },
-        extensions: [StarterKit],
+        extensions: [StarterKit, TaskList, TaskItem.configure({
+            nested: true,
+        }),Table,TableRow, TableCell, TableHeader, Image, ImageResize],
         content: '<p>Hello World! 🌍</p>',
     });
     return (
