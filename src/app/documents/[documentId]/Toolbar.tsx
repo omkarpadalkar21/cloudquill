30b01007f60dbcd8497970e7da7b2635a023b44c
Omkar <omkarpadalkar21@gmail.com>
2025-06-20 19:52:15 +0530

    Add Font Heading

--- src/app/documents/[documentId]/Toolbar.tsx
+++ src/app/documents/[documentId]/Toolbar.tsx
@@ -22,6 +22,69 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { isActive } from "@tiptap/core";
+import { Level } from "@tiptap/extension-heading";
+
+const HeadingLevelButton = () => {
+  const { editor } = useEditorStore();
+  const headings = [
+    { label: "Normal Text", value: 0, fontSize: "16px" },
+    { label: "Heading 1", value: 1, fontSize: "32px" },
+    { label: "Heading 2", value: 2, fontSize: "24px" },
+    { label: "Heading 3", value: 3, fontSize: "20px" },
+    { label: "Heading 4", value: 4, fontSize: "18px" },
+    { label: "Heading 5", value: 5, fontSize: "16px" },
+  ];
+
+  const getCurrentHeading = () => {
+    for (let level = 1; level <= 5; level++) {
+      if (editor?.isActive("heading", {level})) {
+        return `Heading ${level}`;
+      }
+    }
+
+    return "Normal Text";
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button
+          className={
+            "h-7 w-[120px] shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm "
+          }
+        >
+          <span className={"truncate"}>{getCurrentHeading()}</span>
+          <ChevronDownIcon className={"ml-2 size-4 shrink-0"} />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className={"p-1 flex flex-col gap-y-1"}>
+        {headings.map(({ label, value, fontSize }) => (
+          <button
+              onClick={()=>{
+                if (value === 0) {
+                  editor?.chain().focus().setParagraph().run();
+                } else {
+                  editor?.chain().focus().setHeading({ level: value as Level }).run();
+                }
+              }}
+            key={label}
+            style={{ fontSize }}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              (value === 0 && !editor?.isActive("heading")) ||
+                (value !== 0 && editor?.isActive("heading", { level: value as Level}))
+                ? "bg-neutral-200/80"
+                : "",
+            )}
+          >
+            {label}
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 const FontFamilyButton = () => {
   const { editor } = useEditorStore();
@@ -32,15 +95,15 @@ const FontFamilyButton = () => {
     },
     {
       label: "Courier New",
-      value: "Courier New, monospace",
+      value: "Courier New",
     },
     {
       label: "Georgia",
-      value: "Georgia, serif",
+      value: "Georgia",
     },
     {
       label: "Times New Roman",
-      value: "Times New Roman', serif",
+      value: "Times New Roman",
     },
     {
       label: "Verdana",
@@ -52,9 +115,8 @@ const FontFamilyButton = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <button
-
           className={
-            "h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm "
+            "h-7 w-[100px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm "
           }
         >
           <span className={"truncate"}>
@@ -66,7 +128,9 @@ const FontFamilyButton = () => {
       <DropdownMenuContent className={"p-1 flex flex-col gap-y-1"}>
         {fonts.map(({ label, value }) => (
           <button
-              onClick={()=>{editor?.chain().focus().setFontFamily(value).run()}}
+            onClick={() => {
+              editor?.chain().focus().setFontFamily(value).run();
+            }}
             key={label}
             className={cn(
               "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
@@ -197,22 +261,22 @@ const Toolbar = () => {
         <ToolbarButton key={item.label} {...item} />
       ))}
       <Separator
-        className="w-px h-6 bg-neutral-300 mx-1"
+        className="w-px h-6 bg-neutral-300 mx-0"
         style={{ display: "inline-block" }}
       />
-      <FontFamilyButton/>
+      <FontFamilyButton />
       <Separator
-        className="w-px h-6 bg-neutral-300 mx-1"
+        className="w-px h-6 bg-neutral-300 mx-0"
         style={{ display: "inline-block" }}
       />
-      {/*TODO: Heading*/}
+      <HeadingLevelButton/>
       <Separator
-        className="w-px h-6 bg-neutral-300 mx-2"
+        className="w-px h-6 bg-neutral-300 mx-0"
         style={{ display: "inline-block" }}
       />
       {/*TODO: Font Size*/}
       <Separator
-        className="w-px h-6 bg-neutral-300 mx-2"
+        className="w-px h-6 bg-neutral-300 mx-0"
         style={{ display: "inline-block" }}
       />
       {sections[1].map((item) => (
@@ -221,7 +285,7 @@ const Toolbar = () => {
       {/*TODO: Text Color*/}
       {/*TODO: Highlight Color*/}
       <Separator
-        className="w-px h-6 bg-neutral-300 mx-2"
+        className="w-px h-6 bg-neutral-300 mx-0"
         style={{ display: "inline-block" }}
       />
       {/*TODO: Link*/}
