24b75f12fba2a11f5ea43e7131107041c1deb4e2
Omkar <omkarpadalkar21@gmail.com>
2025-06-20 17:20:24 +0530

    Add Editor Tools

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -2,16 +2,16 @@ import Editor from "@/app/documents/[documentId]/editor";
 import Toolbar from "@/app/documents/[documentId]/Toolbar";
 
 interface DocumentIdPageProps {
-    params: Promise<{ documentId: string }>;
+  params: Promise<{ documentId: string }>;
 }
 
-const DocumentIdPage = async ({params}: DocumentIdPageProps) => {
-    const {documentId} = await params;
-    return (
-        <div className={"min-h-screen bg-[#FAFBFD]"}>
-            <Toolbar/>
-            <Editor/>
-        </div>
-    )
-}
-export default DocumentIdPage
+const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
+  return (
+    <div className={"min-h-screen bg-[#FAFBFD]"}>
+      <Toolbar />
+      <Editor />
+    </div>
+  );
+};
+export default DocumentIdPage;
